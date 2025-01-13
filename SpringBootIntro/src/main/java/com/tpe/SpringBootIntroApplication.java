42d8cf8dd2324ca48ca3797650d4dde749387dd0
EkremTnckr <ekremtunckir@gmail.com>
2025-01-13 19:45:15 +0300

    CRUD

--- SpringBootIntro/src/main/java/com/tpe/SpringBootIntroApplication.java
+++ SpringBootIntro/src/main/java/com/tpe/SpringBootIntroApplication.java
@@ -4,17 +4,18 @@ import com.tpe.domain.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-//componentscann bu classiin icinde bulundugu package default olarak tarar.
-//bu classin icindeki package ve altindaki package leri tarar.
+@SpringBootApplication //configuration otomatik olarak gerceklestirilir
+//componetScan bu classin icinde bulundugu packageda default olarak tarar
 public class SpringBootIntroApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootIntroApplication.class, args);
+        //burada tomcat iceride entegre oldugu icin tomcat burdan calistiriliyo
+		/*Student student = new Student();
+		student.getEmail();
+		student.setName(""); //direk burdan ulastik gormek icin bunu kullandik
+		student.setId;*/ //HATA: burda  setter olusturmasina izin vermedigimiz icin kullanamadik
 
-//        Student student = new Student();
-//        student.getEmail();
-//        student.setName("Ali");
 
     }
 
